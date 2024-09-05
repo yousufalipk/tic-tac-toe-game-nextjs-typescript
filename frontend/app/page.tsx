1f25866bfd0b64a123cc3d44e47c547bb91eff5f
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-09-06 00:34:16 +0500

    Components folder moved outside app router!

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,17 @@
-import Board from './components/Board/page';
-
+import { NextPage } from 'next';
+import Board from '../components/Board/page';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <div className='flex justify-center items-center m-20'> 
-    <ToastContainer />
-      <Board />
-    </div>
+    <>
+      <div className='flex justify-center items-center m-20'> 
+        <ToastContainer />
+        <Board />
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
